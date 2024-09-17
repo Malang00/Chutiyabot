b488f135dd2105cfde1720f5a22e97c5762d328c
Jaat <91902331+jaat9512@users.noreply.github.com>
2024-09-17 20:43:22 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,6 +1,6 @@
 api_id = "8006372"
 api_hash = "f878ef2fd1044167b7d8ab23320e1eda"
-bot_token = ""
+bot_token = "7286527244:AAF-CHx3wiI3V9ivoq8dgOVgCPpMReW0j-k"
 auth_users = [1924424042]
 sudo_user = 1924424042
 log_channel = -1001939394872
