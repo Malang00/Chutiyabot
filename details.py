068a5a35488a8a6a38ca8c0fa083367ad5087229
Jaat <91902331+jaat9512@users.noreply.github.com>
2025-03-21 10:41:08 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,7 +1,7 @@
 api_id = "8769548"
 api_hash = "12afe5871629b7062d80f8ef8e274bf6"
-bot_token = "7598656107:AAECSsO4qEbSMp9Zx60Q7VnTG9b2MqL8u6U"
+bot_token = "7658455944:AAFOZjBLX0Eph-ygxB_aHHFw4bEzqW83aiA"
 auth_users = [1925539960]
 sudo_user = 1925539960
-log_channel = -1002413292280
-txt_channel = -1002413292280
+log_channel = -1002487415776
+txt_channel = -1002487415776
